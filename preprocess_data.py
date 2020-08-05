60ec07a03b2a7f8b336de1fa55d71d0db09f23d5
loukiaconstantinou <66959096+loukiaconstantinou@users.noreply.github.com>
2020-08-05 09:07:32 +0100

    added comments
    might need some help to add further comments

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,9 +1,11 @@
+#import libraries
 import pandas as pd
 import os
 from functools import reduce
 import ast
 import numpy as np
 
+#parser function
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser()
@@ -13,7 +15,7 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-
+#gets the correct path depending on which user's data we want to analyse
 if __name__ == "__main__":
     args = parse_args()
     folderpath = args.datapath
@@ -37,19 +39,20 @@ if __name__ == "__main__":
         if not item.startswith('.') and os.path.isfile(os.path.join(folderpath, item)):
             new_file = os.path.join(folderpath, item)
             filepaths.append(new_file)
-
+    
+    #split the path name twice to get the unique name to name our excel files
     for path in filepaths:
             xl_name = path.split(folderpath)
             xl_name2 = xl_name[1].split('.')
             pd.read_json("{}".format(path)).to_excel("{}/{}.xlsx".format(outdir, xl_name2[0]))
 
-    folderpath2 = r"{}".format(outdir) # make sure to put the 'r' in front
+    folderpath2 = r"{}".format(outdir) #make sure to put the 'r' in front
     filepaths2  = [os.path.join(folderpath2, name) for name in os.listdir(folderpath2)]
 
     if not os.path.isdir("combined_csv_files_{}".format(args.name)):
         os.makedirs("combined_csv_files_{}".format(args.name))
 
-    outdir2 = "combined_csv_files_{}".format(args.name) #this is the output dir I'm going to use
+    outdir2 = "combined_csv_files_{}".format(args.name)
 
     dictionary = {}  
     for path in filepaths2:  
@@ -61,15 +64,13 @@ if __name__ == "__main__":
         
     for key in dictionary:
         excel_names = dictionary[key]
-        # read them in
+        #read the excel files
         excels = [pd.ExcelFile(name) for name in excel_names]
-        # turn them into dataframes
+        #turn them into dataframes
         frames = [x.parse(x.sheet_names[0], header=None,index_col=None) for x in excels]
-        # delete the first row for all frames except the first
-        # i.e. remove the header row -- assumes it's the first
+        #delete the first row for all frames except the first (i.e. remove the header row - assumes it's the first)
         frames[1:] = [df[1:] for df in frames[1:]]
-        # concatenate them..
+        #concatenate all the frames
         combined = pd.concat(frames)
-        # write it out
-        # combined.to_excel("{}/combined_{}.xlsx".format(outdir2, key), header=False, index=False)
+        # write it out into csv 
         combined.to_csv("{}/combined_{}.csv".format(outdir2, key), header=False, index=False)
